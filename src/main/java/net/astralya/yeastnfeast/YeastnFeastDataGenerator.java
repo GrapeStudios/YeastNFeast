0f36056b549ce978f53c0828fcfd737adb0e7c15
SirColor <macaraballo12@gmail.com>
2025-09-07 00:49:36 -0400

    Initial Commit

---
+++ src/main/java/net/astralya/yeastnfeast/YeastnFeastDataGenerator.java
@@ -0,0 +1,11 @@
+package net.astralya.yeastnfeast;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YeastnFeastDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
